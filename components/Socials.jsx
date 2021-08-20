d5e9b857eb2dc78c42d02a1a53286cf2aed842bf
starbvuks <starbvuks@gmail.com>
2021-08-20 22:58:07 +0530

    responsive tweaks

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Socials = () => {
   return (
-    <div class="pr-32 mt-20 flex items-center">
-      <button class="font-poppins font-semibold text-xl p-4 text-blue-100 transition-colors duration-150 bg-lightblue rounded-full hover:bg-deepblue ring-2 ring-blue-300 animate-bounce">
+    <div class="sm:pr-32 mt-10 sm:mt-20 flex justify-center sm:justify-start">
+      <button class="font-poppins font-semibold text-md sm:text-xl p-2 sm:p-4 text-blue-100 transition-colors duration-150 bg-lightblue rounded-full hover:bg-deepblue ring-2 ring-blue-300 animate-bounce">
         Check My work
       </button>
     </div>
