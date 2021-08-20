b5697a531f5245cfab450837fa82f6e844cbe6bc
starbvuks <starbvuks@gmail.com>
2021-08-20 23:32:35 +0530

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html, './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'",
+  ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
