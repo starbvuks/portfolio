e5ae4a38a7ffd3bc8fceffc22d244c3ed7c09706
starbvuks <starbvuks@gmail.com>
2021-08-20 21:33:02 +0530

    flex fix

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,11 +2,8 @@ import React from "react";
 
 const Socials = () => {
   return (
-    <div class="pl-24 pr-32 mt-20 flex items-center">
-      <button
-        class="font-poppins font-semibold text-xl p-4 text-blue-100 transition-colors duration-150 bg-lightblue rounded-full hover:bg-deepblue ring-2 ring-blue-300 animate-bounce"
-        id="btn-bounce"
-      >
+    <div class="pr-32 mt-20 flex items-center">
+      <button class="font-poppins font-semibold text-xl p-4 text-blue-100 transition-colors duration-150 bg-lightblue rounded-full hover:bg-deepblue ring-2 ring-blue-300 animate-bounce">
         Check My work
       </button>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -11,42 +11,45 @@ export default function Home() {
   return (
     <div class="h-screen bg-deepblue p-4">
       <Navbar />
-      <div class="pl-24 pr-32 mt-20 h-80 flex items-center flex-col sm:flex-row">
-        <div>
-          <h3 class="font-poppins text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-blue-50">
-            my name's Sarvag
-          </h3>
-          <p class="font-poppins pt-14 leading-relaxed text-blue-100 w-full sm:w-10/12 text-sm md:text-lg lg:text-xl">
-            i&apos;m a Web Dev living in Hyderabad, India currently balancing
-            college and my passion for programming and ui design. Over the past
-            year i&apos;ve spent my time taking courses and learning, notably{" "}
-            <a
-              class="italic text-blue-300"
-              href="https://cs50.harvard.edu/x/2021/"
-            >
-              Harvard&apos;s CS50
-            </a>{" "}
-            and more recently finishing a Full Stack Bootcamp course at{" "}
-            <a
-              class="italic text-blue-300"
-              href="https://www.schoolofacceleratedlearning.com/"
-            >
-              SOAL
-            </a>
-            .
-          </p>
+      <div class="pl-24 pr-32 mt-20 h-80 items-center ">
+        <div class="flex flex-col sm:flex-row">
+          <div>
+            <h3 class="font-poppins text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-blue-50">
+              my name's Sarvag
+            </h3>
+            <p class="font-poppins pt-14 leading-relaxed text-blue-100 w-full sm:w-10/12 text-sm md:text-lg lg:text-xl">
+              i&apos;m a Web Dev living in Hyderabad, India currently balancing
+              college and my passion for programming and ui design. Over the
+              past year i&apos;ve spent my time taking courses and learning,
+              notably{" "}
+              <a
+                class="italic text-blue-300"
+                href="https://cs50.harvard.edu/x/2021/"
+              >
+                Harvard&apos;s CS50
+              </a>{" "}
+              and more recently finishing a Full Stack Bootcamp course at{" "}
+              <a
+                class="italic text-blue-300"
+                href="https://www.schoolofacceleratedlearning.com/"
+              >
+                SOAL
+              </a>
+              .
+            </p>
+          </div>
+          <div class="flex items-center">
+            <Image
+              src="/img/jpg.jpg"
+              alt="me"
+              width={750}
+              height={750}
+              class="object-contain shadow-2xl rounded-3xl invisible sm:visible md:w-5/6 sm:h-full"
+            />
+          </div>
         </div>
-        <div class="flex items-center">
-          <Image
-            src="/img/jpg.jpg"
-            alt="me"
-            width={750}
-            height={750}
-            class="object-contain shadow-2xl rounded-3xl hidden sm:block md:w-5/6 sm:h-full"
-          />
-        </div>
+        <Socials />
       </div>
-      <Socials />
     </div>
   );
 }
