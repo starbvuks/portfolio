6ced96b4e0ae36c70c05bae3159f06f0a1f783d5
Sarvag Kalari <starbvuks@gmail.com>
2021-08-25 18:12:26 +0530

    projects basic all done

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,15 +1,24 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
 import Navbar from "../components/Navbar/NavbarPortf";
 
-import getProjects from "../utils/projects.js";
+import firebase from "../utils/firebase.js";
 
 const projects = () => {
   const langs = ["react", "next js", "material-ui", "tailwind css"];
-  const projects = ["My Movie Database", "Landmark Locator", "Congo Store"];
+  const [projectData, setProjectData] = useState([]);
 
   useEffect(() => {
-    getProjects();
-  });
+    const fetchData = () => {
+      const db = firebase.firestore();
+      db.collection("projects").onSnapshot((snapshot) => {
+        let proj = [];
+        proj = snapshot.docs.map((doc) => doc.data());
+        setProjectData(proj);
+      });
+    };
+    fetchData();
+  }, []);
 
   return (
     <div class="h-full bg-portfGreen pb-10 2xl:pb-44 overflow-hidden">
@@ -29,17 +38,39 @@ const projects = () => {
           ))}
         </div>
       </div>
-      <div class="w-full flex flex-col flex-wrap sm:flex-row items-center my-20 sm:mx-28 gap-7">
-        {projects.map((data, index) => (
-          <button
-            key={index}
-            class="flex bg-portfLightGreen items-end justify-center shadow-md rounded-2xl mt-9 p-5 h-64 w-11/12 sm:w-1/4"
-          >
-            <span class="text-center font-spartan font-bold text-2xl text-portfDark">
-              {data}
-            </span>
-          </button>
-        ))}
+      <div class="w-full flex flex-col flex-wrap sm:flex-row items-center my-20 sm:mx-28 gap-12">
+        {projectData.length > 0 &&
+          projectData.map((data, index) => (
+            <button
+              key={index}
+              class=" bg-portfLightGreen shadow-md rounded-2xl mt-9 w-3/4 sm:w-1/4 transform hover:scale-110 duration-300"
+            >
+              <div class="relative">
+                <div class=" flex flex-col items-center justify-center absolute inset-0 opacity-0 hover:opacity-100 duration-200 hover:bg-portfLightGreen rounded-2xl">
+                  <span class="font-spartan font-bold text-2xl text-portfDark mb-3">
+                    {data.name}
+                  </span>
+                  <span class="font-spartan font-normal text-lg text-portfDark mb-14">
+                    {data.description}
+                  </span>
+                  <div class="w-8 h-8">
+                    <a href="https://landmark-locator.vercel.app/">
+                      <Icon
+                        icon="akar-icons:github-fill"
+                        width="full"
+                        height="full"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <img
+                  src={data.image}
+                  alt="preview"
+                  class="rounded-2xl object-cover"
+                />
+              </div>
+            </button>
+          ))}
       </div>
     </div>
   );
--- utils/projects.js
+++
@@ -1,8 +0,0 @@
-import firebase from "./firebase.js"
-
-const getProjects = async () => {
-    const snapshot = await firebase.firestore().collection("projects").get()
-    snapshot.docs.forEach((doc) => console.log(doc.data()))
-}
-
-export default getProjects
